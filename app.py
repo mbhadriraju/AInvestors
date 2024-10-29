37449ac7421304c9b2731eacca5c5eae7ec9e771
mbhadriraju <151967712+mbhadriraju@users.noreply.github.com>
2024-10-29 14:53:33 +0000

    edits

--- app.py
+++ app.py
@@ -29,10 +29,10 @@ def index():
             if strat:
                 data = pd.read_csv("database/StratDescriptions.csv")
                 # Initialize TF-IDF Vectorizer
-                preprocessed = preprocessor(data)
-                outputs = encoder(preprocessed)
-                sequence_output = outputs["sequence_output"] 
-                X = sequence_output['Description']
+                vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
+                # Transform the text data to feature vectors
+                X = vectorizer.fit_transform(data['Description'])
+                # Labels
                 y = data["Type"]
                 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=101)
                 rfmod = RandomForestClassifier(oob_score=True)
